dc1512c9bb12aa0b2e892c796894bdcda2028420
Adriana Tcholakova <s3550168@student.rmit.edu.au>
2017-09-13 20:57:53 +1000

    pagination

--- public/js/ingredientsController.js
+++ public/js/ingredientsController.js
@@ -2,10 +2,15 @@
  * Created by Brendan on 9/09/2017.
  */
 (function($, w){
+
+    var currentPageIndex = 1;
+
     w.ingredientsController = {
         selectedIngredients: [],
         appendIngredientToURLString: '&allowedIngredient=',
         watch: function(){
+
+            // on dropdown click update display + get json
             $('.li-ingredient').click(function(e){
                 var ingredientName = $(e.target).attr('data-name');
                 var ingString = w.ingredientsController.appendIngredientToURLString.concat(ingredientName);
@@ -18,7 +23,14 @@
                 }
                 updateDisplay();
 
+            });
 
+            // on page button update display + get json for page index
+            $('.li-page').click(function(e){
+
+                currentPageIndex = $(this).text();
+                alert(currentPageIndex);
+                updateDisplay();
             });
         }
 
@@ -35,6 +47,13 @@
         }
     };
 
+    var getQueryString = function ( field, url ) {
+        var href = url ? url : window.location.href;
+        var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+        var string = reg.exec(href);
+        return string ? string[1] : null;
+    };
+
     function updateDisplay(){
         var displayIngredientsUl = $('.selected-ingredients-anchor ul:first');
         var apiP = $('.api-key');
@@ -42,7 +61,10 @@
 
         var apiString = '';
 
+        // clear html containers
         $(displayIngredientsUl).empty();
+        $('#json-results').empty();
+        $("#json-results-count").empty();
 
         var ingredientsList = w.ingredientsController.selectedIngredients;
 
@@ -51,19 +73,41 @@
             apiString += ingredientsList[i];
         }
 
-        apiString += "&maxResult=2&start=10";
+        // apiString += "&maxResult=2&start=10";
 
         $(apiP).text(apiKey + apiString);
+
+        // use the saved page index
+        var pageIndex = currentPageIndex;
+        /*var queryPage = getQueryString("page");
+        if(queryPage) {
+            pageIndex = queryPage;
+        }*/
+        var apiUrl = apiKey.concat(apiString);
+
+        console.log("page: ", pageIndex);
+        console.log("apiUrl: ", apiUrl);
         
         // makes the call to our php controller, which then hits the api
         $.ajax({
             url: apiP.attr('data-api-controller-url'),
             type: 'POST',
             data: {
-                apiUrl: apiKey.concat(apiString)
+                apiUrl: apiUrl,
+                page: pageIndex
             },
             success: function(response){
-                $('#json-results').text(JSON.stringify(response.data.matches));
+
+                var matchesJson = response.data.matches
+                //$('#json-results').text(JSON.stringify(matchesJson));
+
+                $("#json-results-count").text(JSON.stringify(response.data.totalMatchCount) + ' results');
+
+                // loop results display in container
+                for(var i = 0; i < matchesJson.length; i++) {
+                    $('#json-results').append( '<h1>'+matchesJson[i].recipeName +'</h1>');
+                }
+
             }
         });
     }
