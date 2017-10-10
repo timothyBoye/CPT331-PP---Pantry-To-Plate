f3f336357bed4dc154236007ca7cb1f565bce71c
Glenn <s3514329@student.rmit.edu.au>
2017-10-10 21:35:30 +0100

    "Basic 404 error page created"

---
+++ resources/views/404.blade.php
@@ -0,0 +1,3 @@
+<!--https://laravel.com/docs/5.5/errors#custom-http-error-pages-->
+<h2>Whoops!</h2>
+<p>Seems like we have a problem there Houston. Try using the back button in your browser to get back to Earth/</p>
