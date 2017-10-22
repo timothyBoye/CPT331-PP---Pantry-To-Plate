4f24d6c44c4750ff98dfda7491586b0cbdacb292
Glenn <s3514329@student.rmit.edu.au>
2017-10-22 10:18:57 +0100

    "Fixed nav icons and spacing"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,32 +58,36 @@
                 <a class="navbar-brand" href="{{ route('home') }}"><img style="	width: 120px;" src="{{ URL::asset('img/logo-one.png') }}" alt="Pantry to Plate Logo"></a>
             </div>
 
+
+            <span>
+
+
             <!--Nav items on the left hand side-->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ route('home') }}" class="glyphicon glyphicon-home" aria-hidden="true"> Home</a></li>
-                    <li><a href="{{ route('about') }}" class="glyphicon glyphicon-book" aria-hidden="true"> About</a></li>
-                    <li><a href="{{ route('contact') }}" class="glyphicon glyphicon-envelope" aria-hidden="true"> Contact</a></li>
+                    <li><span><a href="{{ route('home') }}" class="glyphicon glyphicon-home" aria-hidden="true"> Home</a></span></li>
+                    <li><span><a href="{{ route('about') }}" class="glyphicon glyphicon-book" aria-hidden="true"> About</a></span></li>
+                    <li><span><a href="{{ route('contact') }}" class="glyphicon glyphicon-envelope" aria-hidden="true"> Contact</a></span></li>
                 </ul>
 
                 <!--Nav items on the right hand side-->
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::guest())
-                        <li><a href="{{ route('login') }}" class="glyphicon glyphicon-log-in" aria-hidden="true"> Login</a></li>
-                        <li> <a href="{{ route('register') }}" class="glyphicon glyphicon-user" aria-hidden="true"> Register</a></li>
+                        <li><span><a href="{{ route('login') }}" class="glyphicon glyphicon-log-in" aria-hidden="true"> Login</a></span></li>
+                        <li><span><a href="{{ route('register') }}" class="glyphicon glyphicon-user" aria-hidden="true"> Register</a></span></li>
                     @else
                         {{--<a href="#"> Hi {{ Auth::user()->name }}</a>--}}
-                        <li><a href="{{ route('profile.cuisines')}}" class="glyphicon glyphicon-cog" aria-hidden="true"> Manage Cuisine Preferences</a></li>
-                        <li><form id="logout-form" action="{{ route('logout') }}" method="POST">
+                        <li><span><a href="{{ route('profile.cuisines')}}" class="glyphicon glyphicon-cog" aria-hidden="true"> Manage Cuisine Preferences</a></span></li>
+                        <li><span><form id="logout-form" action="{{ route('logout') }}" method="POST">
                                 {{ csrf_field() }}
-                            </form></li>
-                        <li><a href="{{Route('profile.saved_recipes')}}" class="glyphicon glyphicon-bookmark" aria-hidden="true"> Saved Recipes</a></li>
+                            </form></span></li>
+                        <li><span><a href="{{Route('profile.saved_recipes')}}" class="glyphicon glyphicon-bookmark" aria-hidden="true"> Saved Recipes</a></span></li>
                     @endif
 
                     @if (Auth::user())
-                        <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="glyphicon glyphicon-log-out" aria-hidden="true"> Logout</a></li>
+                        <li><span><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="glyphicon glyphicon-log-out" aria-hidden="true"> Logout</a></span></li>
                         @if (Auth::user()->role->user_role_name == "Admin")
-                            <li><a href="{{ route('admin') }}">Admin Dashboard</a></li>
+                            <li><span><a href="{{ route('admin') }}">Admin Dashboard</a></span></li>
                         @endif
                     @endif
                 </ul>
