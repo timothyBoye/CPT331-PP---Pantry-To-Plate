de4732802dce8917310261b7ba3ffb0e21a1fce2
jenaeheight <31491545+jenaeheight@users.noreply.github.com>
2017-10-27 16:29:24 +1100

    Added additional comments to misc blade files

--- resources/views/partials/nutritionalInfoPanel.blade.php
+++ resources/views/partials/nutritionalInfoPanel.blade.php
@@ -1,17 +1,20 @@
+<!-- Displays nutritional info table on recipe page -->
 <div>
     <table class="table table-responsive borderless">
         <caption class = "nutritional-table-heading">NUTRITION</caption>
-
+        <!-- Column headings -->
         <thead>
             <tr class="uppercase">
                 <th>Calories</th>
                 <th>Protein</th>
                 <th>Total Fat</th>
+                <!-- Column headers - desktop view -->
                 <th class=" show-desktop">Carbohydrates</th>
                 <th class="show-desktop">Fiber </th>
                 <th class="show-desktop">Sodium </th>
             </tr>
         </thead>
+        <!-- Nutrient details -->
         <tbody>
             <tr class = "green-text">
                 <td>{{$nutritional_info_panel->calories}}</td>
@@ -21,6 +24,7 @@
                 <td class="show-desktop">{{$nutritional_info_panel->gram_fiber == null ? 'unknown' : $nutritional_info_panel->gram_fiber}}g</td>
                 <td class="show-desktop">{{$nutritional_info_panel->mg_sodium == null ? 'unknown' : $nutritional_info_panel->mg_sodium}}mg</td>
             </tr>
+            <!-- Additional breakdown for fats and sugars -->
             <tr>
                 <td></td>
                 <td></td>
@@ -30,6 +34,7 @@
                 <td class="show-desktop"></td>
             </tr>
         </tbody>
+        <!-- Second header row for more compact mobile view -->
         <thead>
         <tr class="uppercase">
             <th class="show-mobile">Carbohydrates</th>
@@ -37,6 +42,7 @@
             <th class="show-mobile">Sodium </th>
         </tr>
         </thead>
+        <!-- Nutrient details -->
         <tbody>
         <tr class = "green-text">
             <td class ="show-mobile">{{$nutritional_info_panel->gram_total_carbohydrates == null ? 'unknown' : $nutritional_info_panel->gram_total_carbohydrates}}g</td>
@@ -44,6 +50,7 @@
             <td class="show-mobile">{{$nutritional_info_panel->mg_sodium == null ? 'unknown' : $nutritional_info_panel->mg_sodium}}mg</td>
         </tr>
         <tr>
+            <!-- Additional breakdown for sugars on mobile -->
             <td class=" small show-mobile">{{$nutritional_info_panel->gram_sugars}} (sugars)</td>
             <td class="show-mobile"></td>
             <td class="show-mobile"></td>
