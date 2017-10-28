2537aba5508ba306154f856d07b19a03254d1f48
Adriana Tcholakova <s3550168@student.rmit.edu.au>
2017-10-28 15:19:24 +1100

    Design changes

--- resources/views/profile/saved-recipes.blade.php
+++ resources/views/profile/saved-recipes.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 
  <div class="container-fluid content">
+     <h2 class=" text-center">Your saved recipes</h2>
     <table id="saved_recipes_table" class="table table-bordered" data-delete-recipe-url="{{ Route('profile.delete_recipe') }}">
         <thead>
             <tr class="saved-recipe-table-heading">
@@ -14,7 +15,9 @@
         <tbody>
             @foreach($mappings as $mapping)
                 <tr>
-                    <td><a href="{{ route('recipe', $mapping->recipe->id) }}">{{$mapping->recipe->name}}</a></td>
+                    <td>
+                        <a href="{{ route('recipe', $mapping->recipe->id) }}">{{$mapping->recipe->name}}</a>
+                    </td>
                     <td>{{$mapping->recipe->short_description}}</td>
                     <td>
                             <button class="delete-saved-recipe-btn btn btn-success" data-recipe-id="{{$mapping->recipe->id}}"><span class="glyphicon glyphicon-remove"></span>Delete</button>
