f792f8e7bcd59f89eefd51019893f94dca1714f7
Adriana Tcholakova <s3550168@student.rmit.edu.au>
2017-10-28 16:59:51 +1100

    Navigation

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,19 +1,27 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-function toggleNav() {
-    var navWidth = parseInt(document.getElementById("mySidenav").style.width);
-    if(navWidth === 300) {
-        closeNav();
-    } else {
-        openNav();
-    }
-}
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "300px";
+// }
+//
+// function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+// }
+//
+// function toggleNav() {
+//     var navWidth = parseInt(document.getElementById("mySidenav").style.width);
+//     if(navWidth === 300) {
+//         closeNav();
+//     } else {
+//         openNav();
+//     }
+// }
+// $(document).ready(function()
+// {
+//     $('ul.nav li a').click(function (e)
+//     {
+//         $('ul.nav li.active').removeClass('active');
+//         $(this).parent('li').addClass('active');
+//     });
+// });
 
 // bootstro: http://clu3.github.io/bootstro.js/#
 $(document).ready(function() {
@@ -28,6 +36,8 @@ $(document).ready(function() {
     });
 
 });
+
+
 
 
 
