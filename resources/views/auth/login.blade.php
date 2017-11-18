b9f41f58497c11c616a36965a5edfa5aead61d02
jenaeheight <31491545+jenaeheight@users.noreply.github.com>
2017-11-18 12:16:04 +1100

    Minor layout adjustment

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <!-- User login page -->
 @section('content')
 <div class="container content">
-    <div class="row">
+    <div class="row login-container">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -2,7 +2,7 @@
 <!-- Password reset page -->
 @section('content')
 <div class="container">
-    <div class="row">
+    <div class="row reset-container">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Reset Password</div>
